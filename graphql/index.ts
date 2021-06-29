56f030808b31e229bd41119cb2e0c8308eb6f37a
Justin Martinez <justinmartinez14@gmail.com>
2021-06-29 15:27:17 -0600

    enables CORS

--- graphql/index.ts
+++ graphql/index.ts
@@ -3,6 +3,7 @@ import { schema } from './schema/schema'
 import Person from './dataSources/Person'
 import { DataSources } from './types/dataSources';
 import Trial from './dataSources/Trial';
+import { Context, HttpRequest } from '@azure/functions';
 
 const server = new ApolloServer({ 
     schema,
@@ -11,4 +12,17 @@ const server = new ApolloServer({
       trial: new Trial()
     })
 });
-exports.graphqlHandler = server.createHandler();
+const graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true
+  }
+});
+
+export default (context: Context, req: HttpRequest) => {
+  // https://github.com/Azure/azure-functions-host/issues/6013
+  req.headers['x-ms-privatelink-id'] = '';
+  // apollo-server only reads this specific string
+  req.headers['Access-Control-Request-Headers'] = req.headers['Access-Control-Request-Headers'] || req.headers['access-control-request-headers'];
+  return graphqlHandler(context, req);
+}
